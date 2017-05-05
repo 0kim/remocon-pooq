8fd464b59d9f5ab21e4a764a2744fab3e3fa43d9
ykim <miricul@gmail.com>
2017-05-05 15:39:48 +0900

    added GA and shell script for dist

--- dist.sh
+++ dist.sh
@@ -1,1 +1,37 @@
-zip -r ./dist/remo_for_pooq_0.2.zip _locales assets js manifest.json popup.html
+VERSION=0.2
+DEPLOY_TARGET=(
+    "_locales"
+    "assets"
+    "js"
+    "manifest.json"
+    "popup.html"
+)
+
+# Check GA_CODE (Google Analytic code)
+if [ $GA_CODE ];
+then
+    echo "GA_CODE($GA_CODE) is used."
+else
+    echo "Please specify GA_CODE variable."
+    exit
+fi
+
+# Clean dist directory
+echo "cleaning dist directory..."
+rm -rf ./dist/*
+
+# Deploy code
+echo "deploying targets to dist directory..."
+for v in ${DEPLOY_TARGET[@]}
+do
+    cp -r $v dist
+done
+
+# Find and Replace ga code
+echo "updating relevant variables...."
+SED_PARAM=s/UA-XXXXXXXX-X/$GA_CODE/g
+sed -i -e $SED_PARAM dist/js/popup.js
+
+# Compress
+cd dist; zip -r remo_for_pooq_$VERSION.zip ${DEPLOY_TARGET[@]}
+
---
+++ js/popup.js
@@ -0,0 +1,11 @@
+var _ga_code = 'UA-XXXXXXXX-X';
+var _gaq = _gaq || [];
+
+_gaq.push(['_setAccount', _ga_code]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
